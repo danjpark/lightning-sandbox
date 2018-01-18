d916554bd1c5211acd56e159aa0b10eebbe7cdf6
Dan Park <danieljinwoopark@gmail.com>
2018-01-18 10:43:42 -0600

    .

--- src/aura/Reg_Component/Reg_ComponentController.js
+++ src/aura/Reg_Component/Reg_ComponentController.js
@@ -1,4 +1,16 @@
 ({
 	myAction : function(component, event, helper) {
+	},
+
+	doSubmit : function(component, event, helper){
+		let intialABC = component.get("v.abc");
+
+		if(intialABC){
+			alert("Value is true");
+			component.set("v.abc", false);
+		}else {
+			alert("Value is false");
+			component.set("v.abc", true);
+		}
 	}
 })
