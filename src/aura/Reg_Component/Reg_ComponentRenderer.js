48d0db69e0f54f42876c16b6684fe4dfb9af64d2
Dan Park <danieljinwoopark@gmail.com>
2018-01-25 18:57:49 -0600

    renderer

--- src/aura/Reg_Component/Reg_ComponentRenderer.js
+++ src/aura/Reg_Component/Reg_ComponentRenderer.js
@@ -1,3 +1,23 @@
 ({
 	// Your renderer method overrides go here
+  render:function(component, helper){
+    //call base render helperMethod
+    //custom rendering
+    let a = this.superRender();
+    console.log("#### from render when comp gets initiated");
+    return a;
+  },
+
+  afterRender:function(component, helper){
+    // call base after render Method
+    this.superAfterRender();
+    //interact with Dom elements
+    console.log("### afterrnder: this text should come after render() is over");
+  },
+
+  rerender:function(component,helper){
+    // call base render
+    this.superRender();
+    console.log("this text is from renderer function.. which gets called whenever data change occuer");
+  }
 })
